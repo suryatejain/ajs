4c5e6f771d7626c6ca2480997c5829b9d3602d57
suryatejain <suryateja.iitkgp@gmail.com>
2014-04-26 11:23:16 +0530

    Update maincontroller.js

--- maincontroller.js
+++ maincontroller.js
@@ -1,6 +1,7 @@
 app.controller("MainController", function($scope,$http){
     $scope.understand = "I now understand how the scope works!";
-    var url = 'data.JSON';
+    delete $http.defaults.headers.common['X-Requested-With'];
+    var url = 'http://in.bookmyshow.com/getHTML.bms?cmd=MOVIESCS';
     $http.get(url).then(function(response){
         $scope.data = response.data;
     })
