24b15e9b709e773b49297b4755bec174ff03d943
suryatejain <suryateja.iitkgp@gmail.com>
2014-04-26 10:59:34 +0530

    Update maincontroller.js

--- maincontroller.js
+++ maincontroller.js
@@ -1,4 +1,4 @@
-app.controller("MainController", function($scope){
+app.controller("MainController", function($scope,$http){
     $scope.understand = "I now understand how the scope works!";
     var url = 'http://in.bookmyshow.com/getHTML.bms?cmd=MOVIESCS';
     $http.get(url).then(function(response){
