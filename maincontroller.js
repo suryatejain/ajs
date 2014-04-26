2ab3c792b93784fce16f292b4a860bdbc8e6a7a0
suryatejain <suryateja.iitkgp@gmail.com>
2014-04-26 11:02:06 +0530

    Update maincontroller.js

--- maincontroller.js
+++ maincontroller.js
@@ -1,6 +1,6 @@
 app.controller("MainController", function($scope,$http){
     $scope.understand = "I now understand how the scope works!";
-    var url = 'http://zuxfer.com/sample.JSON';
+    var url = 'data.JSON';
     $http.get(url).then(function(response){
         console.log(response)
         $scope.data = resonse.data;
